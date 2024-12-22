a910324b3ba6149dd8532c30a071714af0ccb68d
Mohamed-Abdelkawy <m7md.3p2wy@gmail.com>
2024-12-22 18:28:00 +0200

    Created a custom pipe to transform cel to fah and vise versa

---
+++ src/app/temperature.pipe.ts
@@ -0,0 +1,36 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'temp',
+  standalone: true,
+})
+export class TemperaturePipe implements PipeTransform {
+  transform(
+    value: string | number,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah'
+  ) {
+    let val: number;
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    let outputTemp: number;
+    let symbol: '℃' | '℉';
+
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+      symbol = '℉';
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+      symbol = '℃';
+    } else {
+      outputTemp = val;
+      symbol = inputType === 'cel' ? '℃' : '℉';
+    }
+
+    return `${outputTemp} ${symbol}`;
+  }
+}
